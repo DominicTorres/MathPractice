7db6a4269823b98f0c64b2dd02fdb7e1af5570c6
Dominic <dtorres243@west-mec.org>
2020-01-24 18:14:17 -0700

    kuygig

--- Math Practice/m_ath.js
+++ Math Practice/m_ath.js
@@ -52,275 +52,82 @@ function range(){
     output.innerHTML = " The range is " + rangeTotal;
 }
 
-function duplicate(){
-    var n1 = doc1.value(0);
-    var n2 = doc1.value(1);
-    var n3 = doc1.value(2);
-    var n4 = doc1.value(3);
-    var n5 = doc1.value(4);
-    var n6 = doc1.value(5);
-    output.innerHTML = "";
-
-    
-}
-
 function order(){
-    var first = parseFloat(doc1.value);
-    var second = parseFloat(doc2.value);
-    var third = parseFloat(doc3.value);
-    var fourth = parseFloat(doc4.value);
-    var fifth = parseFloat(doc5.value);
-    var sixth = parseFloat(doc6.value);
+    var first = parseFloat(num1.value);
+    var second = parseFloat(num2.value);
+    var third = parseFloat(num3.value);
+    var fourth = parseFloat(num4.value);
+    var fifth = parseFloat(num5.value);
+    var sixth = parseFloat(num6.value);
 
-    var o1 = 0;
-    var o2 = 0;
-    var o3 = 0;
-    var o4 = 0;
-    var o5 = 0;
-    var o6 = 0;
-    
-o1 = math.min(first, second, third, fourth, fifth, sixth);
+    var o1 = 1;
+    var o2 = 1;
+    var o3 = 1;
+    var o4 = 1;
+    var o5 = 1;
+    var o6 = 1;
+
+    o1 = Math.min(first, second, third, fourth, fifth, sixth);
     if(o1 === first){
-        second = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o2 === second){
-        first = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-
-    }else if(o3 === third){
-        first = first;
-        second = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o4 === fourth){
-        first = first;
-        second = second;
-        third = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o5 === fifth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        sixth = fifth;
-    }else if(o6 === sixth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        fifth = fifth;
+        first = second;
+        second = third;
+        third = fourth;
+        fourth = fifth;
+        fifth = sixth;
+    }else if(o1 === second){
+        second = third;
+        third = fourth;
+        fourth = fifth;
+        fifth = sixth;
+    }else if(o1 === third){
+        third = fourth;
+        fourth = fifth;
+        fifth = sixth;
+    }else if(o1 === fourth){
+        fourth = fifth;
+        fifth = sixth;
+    }else if(o1 === fifth){
+        fifth = sixth;
     }
-}
-
-o2 = math.min(first, second, third, fourth, fifth){
-    if(o1 === first){
-        second = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
+    o2 = Math.min(first, second, third, fourth, fifth);
+    if(o2 === first){
+        first = second;
+        second = third;
+        third = fourth;
+        fourth = fifth;
     }else if(o2 === second){
-        first = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-
-    }else if(o3 === third){
-        first = first;
-        second = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o4 === fourth){
-        first = first;
-        second = second;
-        third = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o5 === fifth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        sixth = fifth;
-    }else if(o6 === sixth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        fifth = fifth;
+        second = third;
+        third = fourth;
+        fourth = fifth;
+    }else if(o2 === third){
+        third = fourth;
+        fourth = fifth;
+    }else if(o2 === fourth){
+        fourth = fifth;
     }
-}
-
-o3 = math.min(first, second, third, fourth, fifth){
-    if(o1 === first){
-        second = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o2 === second){
-        first = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-
+    o3 = Math.min(first, second, third, fourth);
+    if(o3 === first){
+        first = second;
+        second = third;
+        third = fourth;
+    }else if(o3 === second){
+        second = third;
+        third = fourth;
     }else if(o3 === third){
-        first = first;
-        second = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o4 === fourth){
-        first = first;
-        second = second;
-        third = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o5 === fifth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        sixth = fifth;
-    }else if(o6 === sixth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        fifth = fifth;
+        third = fourth;
     }
-}
-
-o4 = math.min(first, second, third, fourth, fifth){
-    if(o1 === first){
-        second = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o2 === second){
-        first = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-
-    }else if(o3 === third){
-        first = first;
-        second = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o4 === fourth){
-        first = first;
-        second = second;
-        third = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o5 === fifth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        sixth = fifth;
-    }else if(o6 === sixth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        fifth = fifth;
+    o4 = Math.min(first, second, third);
+    if(o4 === first){
+        first = second;
+        second = third;
+    }else if(o4 === second){
+        second = third;
     }
-}
-
-o5 = math.min(first, second, third, fourth, fifth){
-    if(o1 === first){
-        second = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o2 === second){
-        first = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-
-    }else if(o3 === third){
-        first = first;
-        second = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o4 === fourth){
-        first = first;
-        second = second;
-        third = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o5 === fifth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        sixth = fifth;
-    }else if(o6 === sixth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        fifth = fifth;
+    o5 = Math.min(first, second);
+    if(o5 === first){
+        first = second;
     }
-}
+    o6 = first
 
-o6 = math.min(first, second, third, fourth, fifth){
-    if(o1 === first){
-        second = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o2 === second){
-        first = first;
-        third = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-
-    }else if(o3 === third){
-        first = first;
-        second = second;
-        fourth = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o4 === fourth){
-        first = first;
-        second = second;
-        third = third;
-        fifth = fourth;
-        sixth = fifth;
-    }else if(o5 === fifth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        sixth = fifth;
-    }else if(o6 === sixth){
-        first = first;
-        second = second;
-        third = third;
-        fourth = fourth;
-        fifth = fifth;
-    }
+    output.innerHTML = "order is this: " + o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5 + ", " + o6;
 }
