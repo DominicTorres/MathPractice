32f80c73a9b608f9b2754cd647647fd261976dcf
DominicTorres <dtorres243@west-mec.org>
2020-01-25 21:20:47 -0700

    almost done

--- Math Practice/m_ath.js
+++ Math Practice/m_ath.js
@@ -132,5 +132,177 @@ function order(){
     output.innerHTML = "order is this: " + o1 + ", " + o2 + ", " + o3 + ", " + o4 + ", " + o5 + ", " + o6;
 }
 
+function mode(){
+    var one = parseFloat(doc1.value); 
+    var two = parseFloat(doc2.value); 
+    var three = parseFloat(doc3.value); 
+    var four = parseFloat(doc4.value); 
+    var five = parseFloat(doc5.value); 
+    var six = parseFloat(doc6.value);
+
+    var n1 = 1;
+    var n2 = 1;
+    var n3 = 1;
+    var n4 = 1;
+    var n5 = 1;
+    var n6 = 1;
+
+    if (one === two){
+        n1 ++;
+    }
+    if (one === three){
+        n1 ++;
+    }
+    if (one === four){
+        n1 ++;
+    }
+    if (one === five){
+        n1 ++;
+    }
+    if (one === six){
+        n1 ++;
+    }
+    if (two === three){
+        n2 ++;
+    }
+    if (two === four){
+        n2 ++;
+    }
+    if (two === five){
+        n2 ++;
+    }
+    if (two === six){
+        n2 ++;
+    }
+    if (three === four){
+        n3 ++;
+    }
+    if (three === five){
+        n3 ++;
+    }
+    if (three === six){
+        n3 ++;
+    }
+    if (four === five){
+        n4 ++;
+    }
+    if (four === six){
+        n4 ++;
+    }
+    if (five === six){
+        n5 ++;
+    }
+    var max = Math.max(n1, n2, n3, n4, n5, n6);
+    if(max === n1){
+        output.innerHTML = "mode is " + one + " and it appeared " + max + " number of times"; 
+    }
+    else if(max === n2){
+        output.innerHTML = "mode is " + two + " and it appeared " + max + " number of times"; 
+    }
+    else if(max === n3){
+        output.innerHTML = "mode is " + three + " and it appeared " + max + " number of times"; 
+    }
+    else if(max === n4){
+        output.innerHTML = "mode is " + four + " and it appeared " + max + " number of times"; 
+    }
+    else if(max === n5){
+        output.innerHTML = "mode is " + five + " and it appeared " + max + " number of times"; 
+    }
+    else if(max === n6){
+        output.innerHTML = "mode is " + six + " and it appeared " + max + " number of times"; 
+    }
+}
+
+function middle(){
+    var first = parseFloat(doc1.value); 
+    var second = parseFloat(doc2.value); 
+    var third = parseFloat(doc3.value); 
+    var fourth = parseFloat(doc4.value); 
+    var fifth = parseFloat(doc5.value); 
+    var sixth = parseFloat(doc6.value);
+    var one = 0;
+    var two = 0;
+    var three = 0;
+    var four = 0;
+    var five = 0;
+    var six = 0;
+    one = Math.min(first, second, third, fourth, fifth, sixth);
+    if(one === first){
+        first = second;
+        second = third;
+        third = fourth;
+        fourth = fifth;
+        fifth = sixth;
+    }
+    else if(one === second){
+        second = third;
+        third = fourth;
+        fourth = fifth;
+        fifth = sixth;
+    }
+    else if(one === third){
+        third = fourth;
+        fourth = fifth;
+        fifth = sixth;
+    }
+    else if(one = fourth){
+        fourth = fifth;
+        fifth = sixth;
+    }
+    else if(one = fifth){
+        fifth = sixth;
+    }
+    two = Math.min(first, second, third, fourth, fifth);
+    if(two === first){
+        first = second;
+        second = third;
+        third = fourth;
+        fourth = fifth;
+    }
+    else if(two === second){
+        second = third;
+        third = fourth;
+        fourth = fifth;
+    }
+    else if(two === third){
+        third = fourth;
+        fourth = fifth;
+    }
+    else if(two = fourth){
+        fourth = fifth;
+    }
+    three = Math.min(first, second, third, fourth);
+    if(three === first){
+        first = second;
+        second = third;
+        third = fourth;
+    }
+    else if(three === second){
+        second = third;
+        third = fourth;
+    }
+    else if(three === third){
+        third = fourth;
+    }
+    four = Math.min(first, second, third);
+    if(four === first){
+        first = second;
+        second = third;
+    }
+    else if(four === second){
+        second = third;
+    }
+    five = Math.min(first, second);
+    if(five === first){
+        first = second;
+    }
+    six = first;
+    var middle = (three + four) / 2;
+    output.innerHTML = "The two middle terms are " + three + " and " + four + ", so the median is " + middle;
+    if(output.innerHTML === "The two middle terms are NaN and NaN, so the median is NaN"){
+        output.innerHTML = "I think you forgot to type in the numbers -_-"
+    }
+}
+
 
 
